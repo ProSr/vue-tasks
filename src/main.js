e4f0262c28f026c24fc72bc3009659508c5a8d87
noshimorimoshi <noshimorimoshi@mail.ru>
2022-06-17 22:09:14 +0000

    1.3.dynamic-attributes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from './tutorial/1.Introduction/2.adding-data/App.svelte'
+import App from './tutorial/1.Introduction/3.dynamic-attributes/App.svelte'
 
 const app = new App({
   target: document.getElementById('app')
---
+++ src/tutorial/1.Introduction/3.dynamic-attributes/App.svelte
@@ -0,0 +1,6 @@
+<script>
+	let src = 'https://svelte.dev/tutorial/image.gif',
+	name = 'Rick Astley';
+</script>
+
+<img {src} alt="{name} dances.">

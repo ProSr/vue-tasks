6fb38f5cb2d6fb64448040108f6476f67c21f2ca
noshimorimoshi <noshimorimoshi@mail.ru>
2022-06-17 21:44:12 +0000

    1.2.Adding-data

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from './tutorial/1.Introduction/1.basics/App.svelte'
+import App from './tutorial/1.Introduction/2.adding-data/App.svelte'
 
 const app = new App({
   target: document.getElementById('app')

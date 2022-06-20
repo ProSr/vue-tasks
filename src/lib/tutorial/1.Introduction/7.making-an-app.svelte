01b4c4609c48909c1eebd51375396f15d47c222d
noshimorimoshi <noshimorimoshi@mail.ru>
2022-06-20 22:10:27 +0000

    1.7.making-an-app.svelte 3 of 3 checked

--- src/lib/tutorial/1.Introduction/7.making-an-app.svelte
+++ src/lib/tutorial/1.Introduction/7.making-an-app.svelte
@@ -1,5 +1,5 @@
 <h1>What now?</h1>
-<div><a href="https://svelte.dev/blog/svelte-for-new-developers"><input type="checkbox" checked="true"˝ name="a" id="a">Svelte for new developers</a></div>
+<div><a href="https://svelte.dev/blog/svelte-for-new-developers"><input type="checkbox" checked="true" name="a" id="a">Svelte for new developers</a></div>
 <div><a href="https://svelte.dev/blog/svelte-3-rethinking-reactivity"><input type="checkbox" checked="true" name="b" id="b">Svelte 3: Rethinking reactivity</a></div>
-<div><a href="https://kit.svelte.dev/"><input type="checkbox" name="b" id="b">sveltekit</a></div>
+<div><a href="https://kit.svelte.dev/"><input type="checkbox" checked="true" name="b" id="b">sveltekit</a></div>
 <br>
